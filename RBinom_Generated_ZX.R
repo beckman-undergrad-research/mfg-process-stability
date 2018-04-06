ca43bc541c806a7e0b4d021a13b59f9608805dc1
Xin <brianzhou013@gmail.com>
2018-04-05 23:04:11 -0400

    Use a lot for loop function here
    Use for loop to generate the stable process, dummy way to generate the unstable processes, and plot the yeild versus stability graph

--- RBinom_Generated_ZX.R
+++ RBinom_Generated_ZX.R
@@ -25,6 +25,118 @@ mean(x)
 # cpk / ppk (stability ratio)
 x.sd / (x.AMR/1.128)
 
+#################################################################
+
+# repeat code below NREP times
+set.seed(100)
+NREP <- 10
+N <- 100
+n <- 50
+p <- 0.9
+FBniom <- matrix(NA, NREP, N)
+BMean <- rep(NA, NREP)
+BSd <- rep(NA, NREP)
+for (i in 1:NREP) {
+  x <- rbinom(N,n,p)
+  FBniom[i,1:N] <- x
+  BMean[i] <- mean(x)
+  BSd[i] <- sd(x)
+}
+
+# Verify each row is the generated raw data
+BMean
+FBniom
+BSd
+mean(FBniom[1,])
+
+# Generate iteration matrix
+x.iter <- matrix(NA,NREP, N)
+for (i in 1:NREP){
+  x.iter[i, 1:N] <- c(NA, FBniom[i,1:(N-1)])
+}
+x.iter
+
+# x.iter[1:NREP, 1:N] <- c(NA, FBniom[1:NREP,1:(N-1)])
+#for (i in 1:NREP){
+#  x.MR <- abs(FBniom[1:NREP,1:N] - x.iter[1:NREP,1:N])
+#  
+#}
+
+# Why the code above not working?
+
+# Generate x moving range
+x.MR <- abs(FBniom - x.iter)
+x.MR
+
+# x.AMR <- c(tapply(x.MR[i, 1:N], mean)) 
+# is the above tapply right?
+
+# Calculate average moving range
+x.AMR <- matrix(NA, NREP, 1)
+for (i in 1:NREP){
+  x.AMR[i,] <- mean(x.MR[i,], na.rm = TRUE)
+}
+x.AMR
+# mean(x.MR[,2], na.rm = TRUE)
+# x.MR
+# (4+2+2+3+1+1+3+0+1+1)/10
+# The comment above is we verified the equation is correct
+
+# Calcualte control limit
+UCL <- BMean + 3*(x.AMR/1.128)
+LCL <- BMean - 3*(x.AMR/1.128)
+
+# Calcualte stability ratio and convert yeild to proportion
+x.meanrat <- BMean/n
+x.meanrat
+x.rat <- BSd/(x.AMR/1.128)
+x.rat <- t(x.rat)
+x.rat
+
+# add a unstable process
+x1 <- c(rbinom(100, 50, 0.6), rbinom(50, 45, 0.9))
+x2 <- c(rbinom(70, 50, 0.7), rbinom(50, 45, 0.4))
+
+# Calcualte everything for x1
+N1 <- length(x1)
+x1.iter <- c(NA, x1[1:(N1-1)])
+x1.MR <- abs(x1-x1.iter)
+x1.AMR <- mean(x1.MR, na.rm = TRUE)
+x1.mean <- mean(x1)
+x1.sd <- sd(x1)
+UCL1 <- x1.mean + 3*(x1.AMR/1.128)
+LCL1 <- x1.mean - 3*(x1.AMR/1.128)
+
+# Calcualte everything for x2
+N2 <- length(x2)
+x2.iter <- c(NA, x2[1:(N2-1)])
+x2.MR <- abs(x2-x2.iter)
+x2.AMR <- mean(x2.MR, na.rm = TRUE)
+x2.mean <- mean(x2)
+x2.sd <- sd(x2)
+UCL2 <- x2.mean + 3*(x2.AMR/1.128)
+LCL2 <- x2.mean - 3*(x2.AMR/1.128)
+
+# Calculate ratio for unstable
+x1rat <- x1.sd / (x1.AMR/1.128)
+x2rat <- x2.sd / (x2.AMR/1.128)
+x1.meanrat <- x1.mean/150
+x2.meanrat <- x2.mean/120
+
+# plot the graph
+plot(x = c(x.rat,x1rat,x2rat) , y= c(x.meanrat,x1.meanrat,
+                                     x2.meanrat), main =
+       "Yeild versus Stability", ylab = "Average Yeild", 
+     xlab = "Stability Ratio")
+
+plot(FBniom[1,1:N], ylim = c(UCL[1] + 2, LCL[1] - 2))
+lines(FBniom[1,1:N])
+abline(h = BMean[1], col = "blue")
+abline(h= UCL[1], col = "red")
+abline(h = LCL[1], col = "red")
+
+#################################################################
+
 # geerate 5 binomial samples
 x1 <- rbinom(100, 50, 0.9)
 x2 <- c(rbinom(100, 50, 0.6), rbinom(50, 45, 0.9))
